0e395001d2fc2eecb95499d8b1e49f67f83cf402
GabriMF <108740750+GabriMF@users.noreply.github.com>
2023-01-10 12:19:02 +0100

    ignore it

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+import PokemonCards from './components/PokemonCards.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="Pokemon App!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <RouterView class="router-link-active"/>
 </template>
 
 <style scoped>
